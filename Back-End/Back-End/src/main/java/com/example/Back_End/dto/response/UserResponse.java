5a49c2a4d8ac5f6457e237f095491f66e6367b5b
tranvutruonghuy <huy.tranvutruong.dev157@gmail.com>
2025-10-15 09:58:14 +0700

    refactor code with Lombok, Mapstruct and response api
    1. Lombok (DI)
    2. Mapstruct (mapping data)
    3. response api

--- Back-End/Back-End/src/main/java/com/example/Back_End/dto/response/UserResponse.java
+++ Back-End/Back-End/src/main/java/com/example/Back_End/dto/response/UserResponse.java
@@ -1,23 +1,18 @@
-package com.example.Back_End.dto.request;
+package com.example.Back_End.dto.response;
 
-import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class UserUpdateRequest {
-
-    @Size(min = 8, message = "INVALID_PASSWORD")
+public class UserResponse {
+    String id;
+    String username;
     String password;
-
     String name;
-
     LocalDate dob;
-
 }
---
+++ Back-End/Back-End/src/main/java/com/example/Back_End/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.example.Back_End.mapper;
+
+
+import com.example.Back_End.dto.request.UserCreationRequest;
+import com.example.Back_End.dto.request.UserUpdateRequest;
+import com.example.Back_End.dto.response.UserResponse;
+import com.example.Back_End.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring") //Inform mapstruct: generate mapper to use in spring (dependency injection)
+public interface UserMapper {
+    User toUser(UserCreationRequest request);
+    UserResponse toUserResponse(User user);
+    void updateUser(@MappingTarget User user, UserUpdateRequest request);
+ }
